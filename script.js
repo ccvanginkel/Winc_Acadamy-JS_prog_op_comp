65e7932b6374c8b59a7478d80b6d8de6d15c05ea
ccvanginkel <34521994+ccvanginkel@users.noreply.github.com>
2021-09-26 12:16:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Winc Academy studenten")
+/*
+// Dit is een grote som
+let multiplcation = 1230941 * 1823794;
+console.log("multiplcation:" , multiplcation);
+let division = 637263 / 54;
+console.log("division:", division);
+*/
+// nee de uitkomst is niet zichtbaar, de regel comment is daar om een commentaar te geven waarom bepaalde code is gebruikt
+// nee met blokcomment kun je regels code uitschakelen en is daarom in dit geval niet gebeurd.
